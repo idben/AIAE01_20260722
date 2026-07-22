5b2f753fb5cff61f3eae5e0160fc9627ffe24598
Ben Chen <idben.chen@gmail.com>
2026-07-22 09:33:57 +0800

    缺失值處理1: 刪除

--- pd_test1.py
+++ pd_test1.py
@@ -12,3 +12,13 @@ print(sorted_df, "\n")
 # 複數欄位排序
 sorted2_df = orders_df.sort_values(["數量", "單價"], ascending=[True, False])
 print(sorted2_df, "\n")
+
+# 找出缺失值
+clubs_df = pd.read_csv("clubs.csv")
+print(clubs_df, "\n")
+print(clubs_df.isna(), "\n")
+print(clubs_df.isna().sum(), "\n")
+
+# 缺失值處理1: 刪除該筆資料
+clean1_df = clubs_df.dropna(subset=["社團", "是否繳費"])
+print(clean1_df, "\n")

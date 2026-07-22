f535b54ce03c756c7beb68f2e1958fe7e847a590
Ben Chen <idben.chen@gmail.com>
2026-07-22 11:15:41 +0800

    python 讀取 postgreSQL 資料做為 pandas DataFrame

--- pg_read04.py
+++ pg_read04.py
@@ -2,7 +2,7 @@ from db import get_conn
 import pandas as pd
 pd.set_option("display.unicode.east_asian_width", True)
 
-# python 讀取 postgreSQL 資料做為 pandas DataFrame 的資料夾源
+# python 讀取 postgreSQL 資料做為 pandas DataFrame
 sql_str = """
 SELECT * FROM products ORDER BY id
 """
@@ -10,5 +10,12 @@ SELECT * FROM products ORDER BY id
 with get_conn() as conn:
     products_df = pd.read_sql_query(sql_str, conn)
 
+products_df = products_df.rename(columns={
+    "id": "編號",
+    "name": "品名",
+    "price": "單價",
+    "stock": "庫存",
+    "category_id": "分類"
+})
 
 print(products_df)

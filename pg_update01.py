fc77423d5d3204e98bf097ee678e09583799792d
Ben Chen <idben.chen@gmail.com>
2026-07-22 16:12:52 +0800

    更新成功失敗的訊息判斷

--- pg_update01.py
+++ pg_update01.py
@@ -6,10 +6,16 @@ from psycopg.rows import dict_row
 sql_str = """
 UPDATE products SET price = %s WHERE id = %s
 """
-params = (2100, 3)
+params = (500, 3)
+
+msg = "更新失敗"
 
 with get_conn() as conn:
     with conn.cursor(row_factory=dict_row) as cursor:
         cursor.execute(sql_str, params)
+        if cursor.rowcount >= 1:
+            msg = "更新成功"
         print(f"受影響的筆數: {cursor.rowcount}")
-    conn.commit()
+    conn.commit() # read 為什麼不需要 conn.commit(), 因為 read 不需要寫入
+
+print(msg)
